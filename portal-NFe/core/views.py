0f981511c43642e34811878d96bc82f9c2f0453d
Daniel Doebber <dmdoebber@inf.ufsm.br>
2018-08-30 02:03:33 +0000

    login and logout

--- portal-NFe/core/views.py
+++ portal-NFe/core/views.py
@@ -1,29 +1,32 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.shortcuts import render
 
 def index(request):
-    #if request.user.is_autenticated:
-    return render(request, 'index.html')
-    #return  render(request, 'base.html')
+    if request.user.is_anonymous:
+        return render(request, 'index.html')
+    return  render(request, 'base.html')
 
 def login(request):
+    context = {}
     if request.method == 'POST':
 
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         user = authenticate(username=username, password=password)
-
         if user is not None:
-            login(request, user)
+            auth_login(request, user)
             return render(request, 'index.html')
         else:
-            error = 'Usuario ou Senha incorretos!'
+            context = {'erro':{
+                                'text':'Usuario ou senha invalidos!',
+                                'type':'danger'
+                                }
+                       }
 
-    return  render(request, 'login.html')
+    return  render(request, 'login.html', context )
 
 def logout(request):
-    logout(request)
-
+    auth_logout(request)
     return render(request, 'login.html')
 
